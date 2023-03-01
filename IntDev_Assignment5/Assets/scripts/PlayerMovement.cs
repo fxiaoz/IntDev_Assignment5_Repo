e03b4b12d852431891f17de941c0f407300fbd5c
Xiao <sz3456@nyu.edu>
2023-03-01 11:53:07 -0500

    players, npcs, key, wall, door, player movement, text boxes set up, collisions don't work

---
+++ IntDev_Assignment5/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public SpriteRenderer myRenderer;
+
+    public float speed = 2f;
+
+    public bool keyCollect;
+    public GameObject keyText;
+    public GameObject textbgkey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyCollect = false;
+        keyText.SetActive(false);
+        textbgkey.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            newPos.y += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            newPos.x -= speed * Time.deltaTime;
+            //myRenderer.flipX = false;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            newPos.y -= speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            newPos.x += speed * Time.deltaTime;
+            //myRenderer.flipX = true;
+        }
+
+        transform.position = newPos;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "orange" && Input.GetKeyDown(KeyCode.Space))
+        {
+            keyText.SetActive(true);
+            textbgkey.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "key")
+        {
+            keyCollect = true;
+            Destroy(collision.gameObject);
+        }
+    }
+}
